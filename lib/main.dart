31d48b598cc1c744882f1df7b5439e910a731b72
yahya almongdie <yahyaalmongdie@gmail.com>
2024-10-09 17:22:17 +0300

    add localizations to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_app/core/functions/on_generate_routes.dart';
 import 'package:fruits_app/features/splash/presentation/view/page/splash_view.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   runApp(const FruitHub());
@@ -11,7 +14,15 @@ class FruitHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale("ar"),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.splashView,
