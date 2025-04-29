c8c8760d77684176dc0dbb9377655d43e972034a
yahya almongdie <yahyaalmongdie@gmail.com>
2025-04-29 19:53:44 +0300

    create signup  text form field

--- lib/features/auth/presentation/view/widget/login_view_body.dart
+++ lib/features/auth/presentation/view/widget/login_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/constants.dart';
 import 'package:fruits_app/core/utils/app_colors.dart';
 import 'package:fruits_app/core/utils/app_images.dart';
 import 'package:fruits_app/core/utils/app_text_styles.dart';
@@ -17,7 +18,7 @@ class LoginViewBody extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
         child: Column(
           children: [
             const SizedBox(
--- lib/features/auth/presentation/view/widget/signup_view_body.dart
+++ lib/features/auth/presentation/view/widget/signup_view_body.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/constants.dart';
+import 'package:fruits_app/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_app/generated/l10n.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              CustomTextFormField(
+                textInputType: TextInputType.name,
+                hintText: S.of(context).full_name,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                textInputType: TextInputType.emailAddress,
+                hintText: S.of(context).email,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                suffixIcon: const Icon(
+                  Icons.remove_red_eye_outlined,
+                  color: Color(0xffC9CECF),
+                ),
+                textInputType: TextInputType.visiblePassword,
+                hintText: S.of(context).password,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
+        ));
   }
 }
