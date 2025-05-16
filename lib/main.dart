f94d2290013a847f579356cd4bce86460f8e1394
yahya almongdie <yahyaalmongdie@gmail.com>
2025-05-16 21:40:55 +0300

    custom bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_app/core/functions/on_generate_routes.dart';
+import 'package:fruits_app/core/services/custom_bloc_observer.dart';
 import 'package:fruits_app/core/services/get_it_service.dart';
 import 'package:fruits_app/core/services/shared_preferences_singlton.dart';
 import 'package:fruits_app/core/utils/app_colors.dart';
@@ -14,6 +16,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = CustomBlocObserver();
   await Prefs.init();
   setupGetIt();
   runApp(const FruitHub());
